3bd92521e63966fd1a3ff1cd8f448a38b7001932
Tong-Chen <chentong_biology@163.com>
2013-03-18 21:30:54 +0800

    Add annotation to bamToWig.py

--- bamToWig.py
+++ bamToWig.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 #from __future__ import division, with_statement
 '''
-Copyright 2010, 陈同 (chentong_biology@163.com).  
-Please see the license file for legal information.
+Copyright 2013, 陈同 (chentong_biology@163.com).  
 ===========================================================
 '''
 __author__ = 'chentong & ct586[9]'
