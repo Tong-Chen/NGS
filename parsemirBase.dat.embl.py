4e291cf7f48349b2f7c7479360752f3a946092a5
Tong-Chen <chentong_biology@163.com>
2013-11-25 21:55:02 +0800

    parse mirBase.dat FOR <parsemirBase.dat.embl.py>

---
+++ parsemirBase.dat.embl.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from __future__ import division, with_statement
+'''
+Copyright 2013, 陈同 (chentong_biology@163.com).  
+===========================================================
+'''
+__author__ = 'chentong & ct586[9]'
+__author_email__ = 'chentong_biology@163.com'
+#=========================================================
+desc = '''
+Functional description
+
+This is designed to parase the miRNA.dat file downloaded from miRbase.
+'''
+
+import sys
+import os
+from time import localtime, strftime 
+timeformat = "%Y-%m-%d %H:%M:%S"
+from optparse import OptionParser as OP
+
+def cmdparameter(argv):
+    if len(argv) == 1:
+        global desc
+        print >>sys.stderr, desc
+        cmd = 'python ' + argv[0] + ' -h'
+        os.system(cmd)
+        sys.exit(1)
+    usages = "%prog -i file"
+    parser = OP(usage=usages)
+    parser.add_option("-i", "--input-file", dest="filein",
+        metavar="FILEIN", help="The miRbase.dat downloaded \
+from miRBase.")
+    parser.add_option("-v", "--verbose", dest="verbose",
+        default=0, help="Show process information")
+    parser.add_option("-d", "--debug", dest="debug",
+        default=False, help="Debug the program")
+    (options, args) = parser.parse_args(argv[1:])
+    assert options.filein != None, "A filename needed for -i"
+    return (options, args)
+#--------------------------------------------------------------------
+
+
+def main():
+    options, args = cmdparameter(sys.argv)
+    #-----------------------------------
+    file = options.filein
+    verbose = options.verbose
+    debug = options.debug
+    #-----------------------------------
+    if file == '-':
+        fh = sys.stdin
+    else:
+        fh = open(file)
+    #--------------------------------
+    for line in fh:
+        if line.startswith('ID'):
+            stem_id = line.split()[1]
+        elif line.startswith('AC'):
+            stem_ac = line.split()[1].strip(';')
+        elif line.startswith("FT   miRNA"):
+            mir_pos = line.split()[2]
+        elif line.startswith('FT') and \
+            line.find('accession="MIMAT') != -1:
+            mir_ac = line.split('"')[1]
+        elif line.startswith('FT') and \
+            line.find('product="') != -1:
+            mir_id = line.split('"')[1]
+            print "%s\t%s\t%s\t%s\t%s" % \
+                (stem_id, stem_ac, mir_id, mir_ac, mir_pos)
+    #-------------END reading file----------
+    #----close file handle for files-----
+    if file != '-':
+        fh.close()
+    #-----------end close fh-----------
+    if verbose:
+        print >>sys.stderr,\
+            "--Successful %s" % strftime(timeformat, localtime())
+if __name__ == '__main__':
+    startTime = strftime(timeformat, localtime())
+    main()
+    endTime = strftime(timeformat, localtime())
+    fh = open('python.log', 'a')
+    print >>fh, "%s\n\tRun time : %s - %s " % \
+        (' '.join(sys.argv), startTime, endTime)
+    fh.close()
+
+
+
