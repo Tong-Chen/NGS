9990fb52de32b8f77a106c51d247f11f11c60adf
Tong-Chen <chentong_biology@163.com>
2014-08-07 09:12:30 +0800

    Add support for multiple input and header output FOR <pasteMultipleFilesSpecialCol.py>

--- pasteMultipleFilesSpecialCol.py
+++ pasteMultipleFilesSpecialCol.py
@@ -20,6 +20,7 @@ Test passed compared with paste --- 20131111
 
 import sys
 import os
+import re
 from time import localtime, strftime 
 timeformat = "%Y-%m-%d %H:%M:%S"
 from optparse import OptionParser as OP
@@ -35,7 +36,8 @@ def cmdparameter(argv):
     parser = OP(usage=usages)
     parser.add_option("-i", "--input-file", dest="filein",
         metavar="FILEIN", help="Multiple files can be separated with \
-<,> like <'file1,file2,file3'>." )
+<,> or space(< >) or both, like <'file1,file2,file3'>, \
+<'file1 file2 file3'> or <'file1' 'file2', 'file3'>." )
     parser.add_option("-l", "--file-label", dest="file_label",
         metavar="FILE_LABEL", help="The labels should have the same \
 format and order as filenames given to <-i>. \
@@ -46,7 +48,8 @@ want this file.")
     parser.add_option("-c", "--col", dest="column",
         default=0, help="Select the columns you want to output. \
 Normally the first column is label column, and will only be output \
-once. Default 0 means output all other columns of all files. \
+once. Default 0 means output only the second column of all files \
+with IDs in the first file. \
 Accept '2,3,4' or '2-5' or '2,3-6,7' to indicate the columns wanted. \
 '2,3-6,7' means column 2-7 will all be output.")
     parser.add_option("-o", "--output", dest="output",
@@ -54,6 +57,8 @@ Accept '2,3,4' or '2-5' or '2,3-6,7' to indicate the columns wanted. \
 pasted files. The line order of output file is the same as first file. \
 Also a file nameed <OUTPUT>.label will be outputed \
 as described in -i.")
+    parser.add_option("-H", "--HEADER", dest="header",
+        default=0, help="Add header to output file. Default FALSE.")
     parser.add_option("-v", "--verbose", dest="verbose",
         default=0, help="Show process information")
     parser.add_option("-d", "--debug", dest="debug",
@@ -67,7 +72,8 @@ as described in -i.")
 def main():
     options, args = cmdparameter(sys.argv)
     #-----------------------------------
-    fileL = options.filein.split(',')
+    fileL = re.split('[, ]*', options.filein.strip())
+    #fileL = options.filein.split(',')
     col = options.column
     if col == 0:
         colL = []
@@ -85,31 +91,42 @@ def main():
     #---------------------------------------------
     output = options.output
     if options.file_label:
-        file_labelL = options.file_label.split(',')
+        #file_labelL = options.file_label.split(',')
+        file_labelL = re.split('[, ]*', options.file_label.strip())
         fh = open(output+".label", 'w')
         print >>fh, '\n'.join(file_labelL)
         fh.close()
+    #--------------------------------
+    header = options.header
     verbose = options.verbose
     debug = options.debug
     #-----------------------------------
     aDict = {}
     labelL = []
     i = 0
+    headerL =[]
     for file in fileL:
         aDict[file] = {}
         i += 1
         for line in open(file):
-            if len(colL) == 0:
+            specifyCol = len(colL)
+            if specifyCol == 0:
                 lineL = line.strip().split("\t", 1)
                 key = lineL[0]
                 if i == 1:
                     labelL.append(key)
+                    if header and not headerL:
+                        headerL = file_labelL
                 value = lineL[1]
             else:
                 lineL = line.strip().split("\t")
                 key = lineL[0]
                 if i == 1:
                     labelL.append(key)
+                    if header and not headerL:
+                        headerL = [i+"_"+str(j) \
+                                    for i in file_label \
+                                    for j in range(specifyCol)] 
                 valueL = [lineL[j] for j in colL]
                 value = '\t'.join(valueL)
             #--------------------------------------
@@ -121,6 +138,8 @@ def main():
         #---------------------------------------------------
     #-------------END reading file----------
     fh = open(output, 'w')
+    if header and headerL:
+        print >>fh, "ID\t%s" % '\t'.join(headerL)
     for key in labelL:
         print >>fh, "%s\t%s" \
             % (key, '\t'.join([aDict[file][key] for file in fileL]))
