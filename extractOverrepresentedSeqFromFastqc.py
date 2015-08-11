f7f1693847a59f18074a41be8fb6b6f1bc61a4a6
Tong-Chen <chentong_biology@163.com>
2015-08-11 14:54:41 +0800

    get overrepresented sequence used for adaptor FOR <extractOverrepresentedSeqFromFastqc.py>

---
+++ extractOverrepresentedSeqFromFastqc.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from __future__ import division, with_statement
+'''
+Copyright 2013, 陈同 (chentong_biology@163.com).  
+===========================================================
+'''
+__author__ = 'chentong & ct586[9]'
+__author_email__ = 'chentong_biology@163.com'
+#=========================================================
+desc = '''
+Functional description:
+    This is used to extract overrepresented sequences from the HTML
+    output of fastqc.
+'''
+
+import sys
+import os
+from json import dumps as json_dumps
+from time import localtime, strftime 
+timeformat = "%Y-%m-%d %H:%M:%S"
+from optparse import OptionParser as OP
+#from multiprocessing.dummy import Pool as ThreadPool
+from bs4 import BeautifulSoup
+
+def fprint(content):
+    print json_dumps(content,indent=1)
+
+def cmdparameter(argv):
+    if len(argv) == 1:
+        global desc
+        print >>sys.stderr, desc
+        cmd = 'python ' + argv[0] + ' -h'
+        os.system(cmd)
+        sys.exit(1)
+    usages = "%prog -i file_fastqc.html"
+    parser = OP(usage=usages)
+    parser.add_option("-i", "--input-file", dest="filein",
+        metavar="FILEIN", help="The HTML output of fastqc.")
+    parser.add_option("-v", "--verbose", dest="verbose",
+        default=0, help="Show process information")
+    parser.add_option("-d", "--debug", dest="debug",
+        default=False, help="Debug the program")
+    (options, args) = parser.parse_args(argv[1:])
+    assert options.filein != None, "A filename needed for -i"
+    return (options, args)
+#--------------------------------------------------------------------
+
+
+def main():
+    options, args = cmdparameter(sys.argv)
+    #-----------------------------------
+    file = options.filein
+    verbose = options.verbose
+    debug = options.debug
+    #-----------------------------------
+    if file == '-':
+        fh = sys.stdin
+    else:
+        fh = open(file)
+    #--------------------------------
+    soup = BeautifulSoup(fh, 'html.parser')
+    table = soup.find_all('table')[1]
+    aDict = {}
+    id = 0
+    for tr in table.find_all('tr'):
+        id += 1
+        aDict[id] = []
+        for td in tr.find_all('td'):
+            aDict[id].append(td.get_text())
+    #-------------END reading file----------
+    keyL = aDict.keys()
+    keyL.sort()
+    for key in keyL:
+        valueL = aDict[key]
+        if len(valueL) >= 1:
+            if 'No Hit' != valueL[3]:
+                print ">%d %s\n%s" % (key, valueL[3], valueL[0])
+    #----close file handle for files-----
+    if file != '-':
+        fh.close()
+    #-----------end close fh-----------
+    ###--------multi-process------------------
+    #pool = ThreadPool(5) # 5 represents thread_num
+    #result = pool.map(func, iterable_object)
+    #pool.close()
+    #pool.join()
+    ###--------multi-process------------------
+    if verbose:
+        print >>sys.stderr,\
+            "--Successful %s" % strftime(timeformat, localtime())
+
+if __name__ == '__main__':
+    startTime = strftime(timeformat, localtime())
+    main()
+    endTime = strftime(timeformat, localtime())
+    fh = open('python.log', 'a')
+    print >>fh, "%s\n\tRun time : %s - %s " % \
+        (' '.join(sys.argv), startTime, endTime)
+    fh.close()
+    ###---------profile the program---------
+    #import profile
+    #profile_output = sys.argv[0]+".prof.txt")
+    #profile.run("main()", profile_output)
+    #import pstats
+    #p = pstats.Stats(profile_output)
+    #p.sort_stats("time").print_stats()
+    ###---------profile the program---------
+
+
