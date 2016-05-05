6733e375dd81080ca7206cb8ea12c6406eb644cd
Tong-Chen <chentong_biology@163.com>
2016-05-06 06:05:55 +0800

    Fetch SRA FOR <sraFetch.sh>

---
+++ sraFetch.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#set -x
+set -e
+set -u
+
+usage()
+{
+cat <<EOF >&2
+${txtcyn}
+Usage:
+
+$0 options${txtrst}
+
+${bldblu}Function${txtrst}:
+
+This script is used to download sra files from a file containing SRA
+accession numbers and transfer SRA to fastq format.
+
+The format of input file:
+
+SRR2952884      Y25_root
+SRR2952883      Y18_root
+SRR2952882      Y12_root
+SRR2952881      Y05_root
+
+The second column will be treated as the prefix of final fastq files.
+
+${txtbld}OPTIONS${txtrst}:
+	-f	Data file with format described above${bldred}[NECESSARY]${txtrst}
+	-z	Is there a header[${bldred}Default TRUE${txtrst}]
+EOF
+}
+
+file=
+
+while getopts "hf:z:" OPTION
+do
+	case $OPTION in
+		h)
+			usage
+			exit 1
+			;;
+		f)
+			file=$OPTARG
+			;;
+		?)
+			usage
+			exit 1
+			;;
+	esac
+done
+
+if [ -z $file ]; then
+	usage
+	exit 1
+fi
+
+
+#IFS="\t"
+
+cat $file | while read -r -a array 
+do
+	sra="${array[0]}"
+	name="${array[1]}"
+	#echo $sra, $name
+	#prefetch -v $sra
+	#prefetch -v $sra
+	#prefetch -v $sra
+	#/bin/cp ~/ncbi/public/sra/${sra}.sra ${name}.sra
+	fastq-dump -v --split-3 --gzip ${sra}
+	#/bin/cp ~/ncbi/public/sra/${sra}* .
+	rename "${sra}" "${name}" ${sra}*
+	/bin/rm ~/ncbi/public/sra/${sra}.sra
+done
