1b6a7391634cf1bb6abfd3bde20642ff8730f3dd
Tong-Chen <chentong_biology@163.com>
2016-08-31 11:15:23 +0800

    parse KEGG key FOR <parseKO00001.keg.py>

---
+++ parseKO00001.keg.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import division, with_statement
+'''
+Copyright 2015, 陈同 (chentong_biology@163.com).  
+===========================================================
+'''
+__author__ = 'chentong & ct586[9]'
+__author_email__ = 'chentong_biology@163.com'
+#=========================================================
+desc = '''
+Program description:
+    This is designed to parse ko00001.keg.
+'''
+
+import sys
+import os
+from json import dumps as json_dumps
+from time import localtime, strftime 
+timeformat = "%Y-%m-%d %H:%M:%S"
+from optparse import OptionParser as OP
+#from multiprocessing.dummy import Pool as ThreadPool
+import re
+
+#from bs4 import BeautifulSoup
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+debug = 0
+
+def fprint(content):
+    """ 
+    This is a Google style docs.
+
+    Args:
+        param1(str): this is the first param
+        param2(int, optional): this is a second param
+            
+    Returns:
+        bool: This is a description of what is returned
+            
+    Raises:
+        KeyError: raises an exception))
+    """
+    print json_dumps(content,indent=1)
+
+def cmdparameter(argv):
+    if len(argv) == 1:
+        global desc
+        print >>sys.stderr, desc
+        cmd = 'python ' + argv[0] + ' -h'
+        os.system(cmd)
+        sys.exit(1)
+    usages = "%prog -i file"
+    parser = OP(usage=usages)
+    parser.add_option("-i", "--input-file", dest="filein",
+        metavar="FILEIN", help="ko00001.keg")
+    parser.add_option("-v", "--verbose", dest="verbose",
+        action="store_true", help="Show process information")
+    parser.add_option("-d", "--debug", dest="debug",
+        default=False, action="store_true", help="Debug the program")
+    (options, args) = parser.parse_args(argv[1:])
+    assert options.filein != None, "A filename needed for -i"
+    return (options, args)
+#--------------------------------------------------------------------
+
+
+def main():
+    options, args = cmdparameter(sys.argv)
+    #-----------------------------------
+    file = options.filein
+    verbose = options.verbose
+    global debug
+    debug = options.debug
+    #-----------------------------------
+    C = re.compile(r"C *(\d+) (.*) \[(.*)\]")
+    D = re.compile(r"D *([^ ]+) *(.*); ([^[]*) *(\[EC:.*\])*")
+
+    if file == '-':
+        fh = sys.stdin
+    else:
+        fh = open(file)
+    #--------------------------------
+    for line in fh:
+        if line[0] == 'C':
+            c  = C.match(line)
+            assert c, line
+            cg = c.groups()
+            cg_line = cg[2] + '\t' + cg[1]
+        elif line[0] == 'D':
+            d = D.match(line)
+            assert d, line
+            dg = d.groups()
+            if dg[3]:
+                dg_line = "\t".join(dg)
+            else:
+                dg_line = "\t".join(dg[:3])
+            print "%s\t%s" % (cg_line, dg_line)
+    #-------------END reading file----------
+    #----close file handle for files-----
+    if file != '-':
+        fh.close()
+    #-----------end close fh-----------
+    ###--------multi-process------------------
+    #pool = ThreadPool(5) # 5 represents thread_num
+    #result = pool.map(func, iterable_object)
+    #pool.close()
+    #pool.join()
+    ###--------multi-process------------------
+    if verbose:
+        print >>sys.stderr,\
+            "--Successful %s" % strftime(timeformat, localtime())
+
+if __name__ == '__main__':
+    startTime = strftime(timeformat, localtime())
+    main()
+    endTime = strftime(timeformat, localtime())
+    fh = open('python.log', 'a')
+    print >>fh, "%s\n\tRun time : %s - %s " % \
+        (' '.join(sys.argv), startTime, endTime)
+    fh.close()
+    ###---------profile the program---------
+    #import profile
+    #profile_output = sys.argv[0]+".prof.txt")
+    #profile.run("main()", profile_output)
+    #import pstats
+    #p = pstats.Stats(profile_output)
+    #p.sort_stats("time").print_stats()
+    ###---------profile the program---------
+
+
