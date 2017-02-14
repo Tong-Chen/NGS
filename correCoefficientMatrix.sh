59d898eae777193712fff198a826eb65228d5131
Tong-Chen <chentong_biology@163.com>
2017-02-14 16:46:04 +0800

    functional update FOR <correCoefficientMatrix.sh>

--- correCoefficientMatrix.sh
+++ correCoefficientMatrix.sh
@@ -24,7 +24,9 @@ The input file is like:
 	4	0.50076958	1.23827656	0.8005727
 
 
-If -t is FALSE, the output is:
+The output has two files:
+
+One is matrix:
 
 	label	1           2          3          4
 	1  1.00000000 -0.03157877 -0.7592889 -0.0333722
@@ -32,9 +34,9 @@ If -t is FALSE, the output is:
 	3 -0.75928887 -0.62645177  1.0000000 -0.6250521
 	4 -0.03337220  0.99999839 -0.6250521  1.0000000
 
-If -t is TRUE, the output is:
+The other contains statistical information:
 
-Var1	Var2	value
+Var1	Var2	value p	adjust_p
 1	1	1.00000000
 2	1	-0.03157877
 3	1	-0.75928887
@@ -58,19 +60,14 @@ The parameters for logical variable are either TRUE or FALSE.
 ${txtbld}OPTIONS${txtrst}:
 	-f	Data file (with header line, the first column is the
  		rowname, all columns are tab seperated)${bldred}[NECESSARY]${txtrst}
-	-s	Scale data by rows.${bldred}[FALSE]${txtrst}
+	-s	Scale data by rows.${bldred}[FALSE, do not change this]${txtrst}
 	-m	The method.[${bldred}Default pearson, accept "kendall",
 		"spearman".${txtrst}]
 	-T	Compute correlation coefficient for all columns instead of all
 		rows.
 		[${bldred}Default FALSE, accept TRUE.${txtrst}]
-	-t	Transfer correlation coefficient matrix to three column file
-		in format like below
-		gene1	gene2	correlation_coefficient
-		A	A	1
-		A	B	0.9
-		A	C	0.5
-		A	D	0.2
+	-p	Plot correlation matrix. 
+		[${bldred}Default FALSE, accept TRUE.${txtrst}]
 	-e	Execute or not[${bldred}Default TRUE${txtrst}]
 	-i	Install the required packages[${bldred}Default FALSE${txtrst}]
 EOF
@@ -80,11 +77,11 @@ file=''
 execute='TRUE'
 ist='FALSE'
 method='pearson'
-transfer='FALSE'
+plot='FALSE'
 col_cor='FALSE'
 scale_row='FALSE'
 
-while getopts "hf:s:t:T:m:e:i:" OPTION
+while getopts "hf:s:p:T:m:e:i:" OPTION
 do
 	case $OPTION in
 		h)
@@ -97,8 +94,8 @@ do
 		s)
 			scale_row=$OPTARG
 			;;
-		t)
-			transfer=$OPTARG
+		p)
+			plot=$OPTARG
 			;;
 		T)
 			col_cor=$OPTARG
@@ -127,9 +124,9 @@ if [ -z $file ] ; then
 fi
 
 if [ "$col_cor" == "FALSE" ]; then
-	midname=".row.${method}.cor"
+	midname=".row_${method}_cor"
 else
-	midname=".col.${method}.cor"
+	midname=".col_${method}_cor"
 fi
 
 cat <<END >${file}${midname}.r
@@ -144,9 +141,15 @@ if (${scale_row}){
 }
 
 if ($col_cor) {
+	if (${scale_row}){
+		data <- t(apply(data, 2, scale))
+	}
 	ct <- corr.test(data, method="$method",adjust="none")
 	#cc <- cor(data, use="everything", method="$method")
 } else {
+	if (${scale_row}){
+		data <- t(apply(data, 1, scale))
+	}
 	ct <- corr.test(t(data), method="$method",adjust="none")
 	#cc <- t(cor(t(data), use="everything", method="$method"))
 }
@@ -160,10 +163,22 @@ result <- subset(result, select=c("pair", "r", "p", "q"))
 
 colnames(result) <- c("Pair", "${method}_correlation", "p_value", "q_value")
 
-write.table(result, file="${file}$midname", sep="\t", col.names=T,
+write.table(result, file="${file}$midname.statistics.xls", sep="\t", col.names=T,
 row.names=F, quote=F)
 
-#if ($transfer){
+write.table(ct\$r, file="${file}$midname.matrix.xls", sep="\t", col.names=T,
+row.names=T, quote=F)
+
+if ($plot){
+	if(${ist}){
+		install.packages("corrplot", repo="http://cran.us.r-project.org")
+	}
+	library(corrplot)
+	pdf("${file}$midname.matrix.pdf")
+	corrplot.mixed(ct\$r, order ="FPC", hclust.method="complete", method="color")
+	dev.off()
+}
+#if ($plot){
 #
 #	if(${ist}){
 #		install.packages("reshape2", repo="http://cran.us.r-project.org")
