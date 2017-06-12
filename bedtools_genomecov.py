8a55f0aa8787deee09923a555af95c713d778211
Tong-Chen <chentong_biology@163.com>
2017-06-12 15:04:17 +0800

    deal with bedtools genome cov output FOR <bedtools_genomecov.py>

---
+++ bedtools_genomecov.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import division, with_statement
+'''
+Copyright 2015, 陈同 (chentong_biology@163.com).  
+===========================================================
+'''
+__author__ = 'chentong & ct586[9]'
+__author_email__ = 'chentong_biology@163.com'
+#=========================================================
+desc = '''
+Program description:
+
+'''
+
+import sys
+import os
+from json import dumps as json_dumps
+from time import localtime, strftime 
+timeformat = "%Y-%m-%d %H:%M:%S"
+from optparse import OptionParser as OP
+#from multiprocessing.dummy import Pool as ThreadPool
+
+#from bs4 import BeautifulSoup
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+debug = 0
+
+def fprint(content):
+    """ 
+    This is a Google style docs.
+
+    Args:
+        param1(str): this is the first param
+        param2(int, optional): this is a second param
+            
+    Returns:
+        bool: This is a description of what is returned
+            
+    Raises:
+        KeyError: raises an exception))
+    """
+    print json_dumps(content,indent=1)
+
+def cmdparameter(argv):
+    if len(argv) == 1:
+        global desc
+        print >>sys.stderr, desc
+        cmd = 'python ' + argv[0] + ' -h'
+        os.system(cmd)
+        sys.exit(1)
+    usages = "%prog -i file"
+    parser = OP(usage=usages)
+    parser.add_option("-i", "--input-file", dest="filein",
+        metavar="FILEIN", help="Bedgraph output of bedtools genomecov")
+    parser.add_option("-l", "--label", dest="label",
+        metavar="FILEIN", help="Label of the file. Normally sample names.")
+    parser.add_option("-v", "--verbose", dest="verbose",
+        action="store_true", help="Show process information")
+    parser.add_option("-D", "--debug", dest="debug",
+        default=False, action="store_true", help="Debug the program")
+    (options, args) = parser.parse_args(argv[1:])
+    assert options.filein != None, "A filename needed for -i"
+    return (options, args)
+#--------------------------------------------------------------------
+
+
+def main():
+    options, args = cmdparameter(sys.argv)
+    #-----------------------------------
+    file = options.filein
+    label = options.label
+    verbose = options.verbose
+    global debug
+    debug = options.debug
+    #-----------------------------------
+    if file == '-':
+        fh = sys.stdin
+    else:
+        fh = open(file)
+    #--------------------------------
+    total_len = 0
+    zero_cov_len = 0
+    cov_len = 0
+    total_cov = 0
+    for line in fh:
+        chr, start, end, cov = line.split()
+        start = int(start)
+        end   = int(end)
+        cov   = int(cov)
+        breadth = end - start
+        total_len += breadth
+        if cov != 0:
+            cov_len += breadth
+            total_cov += cov*breadth
+    #-------------END reading file----------
+    print >>sys.stderr, total_cov
+    print >>sys.stderr, cov_len
+    print >>sys.stderr, total_len
+    print "variable\tvalue\tsample"
+    print "{}\t{}\t{}".format("Reads sheathed genome regions (%)",
+            100*cov_len/total_len, label)
+    print "{}\t{}\t{}".format("Sequencing depth relative to whole genome", 
+            total_cov/total_len, label)
+    print "{}\t{}\t{}".format("Sequencing depth relative to sheathed genome", 
+            total_cov/cov_len, label)
+    #----close file handle for files-----
+    if file != '-':
+        fh.close()
+    #-----------end close fh-----------
+    ###--------multi-process------------------
+    #pool = ThreadPool(5) # 5 represents thread_num
+    #result = pool.map(func, iterable_object)
+    #pool.close()
+    #pool.join()
+    ###--------multi-process------------------
+    if verbose:
+        print >>sys.stderr,\
+            "--Successful %s" % strftime(timeformat, localtime())
+
+if __name__ == '__main__':
+    startTime = strftime(timeformat, localtime())
+    main()
+    endTime = strftime(timeformat, localtime())
+    fh = open('python.log', 'a')
+    print >>fh, "%s\n\tRun time : %s - %s " % \
+        (' '.join(sys.argv), startTime, endTime)
+    fh.close()
+    ###---------profile the program---------
+    #import profile
+    #profile_output = sys.argv[0]+".prof.txt")
+    #profile.run("main()", profile_output)
+    #import pstats
+    #p = pstats.Stats(profile_output)
+    #p.sort_stats("time").print_stats()
+    ###---------profile the program---------
+
+
